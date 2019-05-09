e8bcc112a874858bbb68b713520e4530217923cc
MorganAsnar <morgan.asnar@insa-lyon.fr>
2019-05-09 15:08:37 +0200

    mise à jour des views, avec les requêtes pour récupérer les ressources à afficher sur la page. Mise à jour des urls avec les pages ajoutées/modifiées.

--- vitrine/urls.py
+++ vitrine/urls.py
@@ -4,8 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('ouvrages', views.ouvrages, name='ouvrages'),
-    path('festival', views.festival, name='festival'),
+    path('recherche-ouvrages', views.rechercheOuvrages, name='recherche-ouvrages'),
+#   path('ouvrage/<str:auteur>/<str:titre>/<int:num_page>', views.ouvrage, name='ouvrage'),
+    path('actus/<str:tag>', views.actus, name='actus'),
+    path('article/<int:id_article>/<int:num_page>', views.article, name='article'),
+    path('festival/<int:num_page>', views.festival, name='festival'),
     path('archives', views.archives, name='archives'),
     path('infos', views.infos, name='infos'),
     path('contact', views.contact, name='contact'),
--- vitrine/views.py
+++ vitrine/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, get_object_or_404
 #pour les modèles
 from vitrine.models import Article, Commentaire, Tag
 
+#Pour des query plus complexes que le set de base
+from django.db.models import Q
+
 #pour les formulaires
 #from .forms import
 
@@ -24,11 +27,76 @@ def index(request):
     return render(request, 'vitrine/index.html', {"last_articles": query})
 
 
-def ouvrages(request):
-    return 1
+def rechercheOuvrages(request):
+    """Page de recherche d'ouvrage par nom/auteur
+
+    :param request : OSEF
+    :type request : requête http
+
+    :return paquet http contenant la page"""
+    return render('vitrine/rechercheOuvrage.html', locals())
 
 
-def festival(request):
+# def ouvrage(request, auteur, titre, num_page):
+#     """Affiche les résultats de la recherche d'ouvrage
+#
+#     :param request : OSEF
+#     :param auteur : auteur recherché
+#     :param titre : titre recherché
+#     :type request : requête http
+#     :type auteur : String, peut être ""
+#     :type auteur : String, peut être ""
+#
+#     :return paquet http contenant la page"""
+#
+#     #Cherche dans la bdd les articles qui contiennent l'auteur et le titre renseigné. Si un des paramètres est vide,
+#     #il n'influera pas sur la recherche Si les deux sont vides, on a toute la Bdd
+#     n=10
+#     query=Ouvrage.objects.filter(Q(auteur__icontains=auteur) & Q(titre__icontains=titre)).order_by("titre")[num_page:num_page+n]
+#     return render(request, 'vitrine/ouvrage.html', {"livres": query, "page": num_page})
+
+
+def actus(request, num_page, tag=""):
+    """La page actus affiche les n dernières actus du site
+
+    :param request : OSEF
+    :param num_page : La page a afficher, définit l'ancienneté des articles qu'on va afficher; les n premiers, puis les n suivants, etc
+        (n entier modulable, définit plus loin)
+    :param tag : Le tag qu'on recherche éventuellement, peut être ""
+    :type request : requête http
+    :type num_page : int
+    :type tag: String
+
+    :return paquet http contenant la page"""
+
+    #Récupération des n articles avec le tag spécifié
+    n = 10
+    query = Article.objects.filter(tag__nom_tag__contains=tag).order_by("-date")[num_page:num_page+n]
+    return render(request, 'vitrine/actus.html', {"last_articles": query, "page" : num_page})
+    #/!\ définition valide pour des affichages page par page, mais apparement aussi pour de l'infinite scroll (cf : https://infinite-scroll.com/)
+
+
+def article(request, id_article, num_page):
+    """page d'un article sépicifique, avec les commentaires en dessous
+
+    page composée de l'article avec l'id_article donné, et des commentaires qui y sont liées, triées par date de publication
+
+    :param request : OSEF
+    :param id_article : l'id de l'article à afficher
+    :param num_page : La page a afficher, définit l'ancienneté des commentaires qu'on va afficher; les n premiers, puis les n suivants, etc
+    :type request : requête HTTP
+    :type id_article : int
+    :type num_page : int
+
+    :return paquet http contenant la page"""
+
+    article=get_object_or_404(Article, id=id_article)
+    n=10
+    query = Commentaire.objects.filter(id_article=id_article).order_by("-date")[num_page:num_page+n]
+    return render(request, 'vitrine/article.html', {"article": article, "commentaires": query, "page": num_page})
+
+
+def festival(request, num_page):
     """page sur le festival et le concours BD
 
         page composée d'une description statique, puis des articles tagués "festival" et "concours"
@@ -37,7 +105,12 @@ def festival(request):
         :type request : requête HTTP
 
         :return paquet http contenant la page"""
-    return 1
+
+    #Récupère les n articles qui ont le tag "festival" ou "concours", met en premier ceux qui sont épinglés, et trie ensuite par date
+    n=10
+    query = Article.objects.filter(Q(tag="festival") | Q(tag="concours")).order_by("epingler", "-date")[num_page:num_page+n]
+    return render(request, 'vitrine/festival.html', {"last_articles": query, "page" : num_page})
+    #/!\ définition valide pour des affichages page par page, mais apparement aussi pour de l'infinite scroll (cf : https://infinite-scroll.com/)
 
 
 def archives(request):
@@ -45,8 +118,24 @@ def archives(request):
 
 
 def infos(request):
-    return 1
+    """Page avec les infos sur le club; plan d'accès, coordonnées, réseaux sociaux...
+
+    Page peu destinée à évoluer, donc tout pas de paramètres spécifiques à lui passer, tout est dans le template
+
+    :param request : OSEF
+    :type request : requête http
+
+    :return paquet http contenant la page"""
+    return render(request, 'vitrine/infos.html', locals())
 
 
 def contact(request):
-    return 1
+    """Page avec un formulaire de contact; un champs remplissable, qui sera envoyé sur une boite mail
+
+    Page peu destinée à évoluer, donc tout pas de paramètres spécifiques à lui passer, tout est dans le template
+
+    :param request : OSEF
+    :type request : requête http
+
+    :return paquet http contenant la page"""
+    return render(request, 'vitrine/contact.html', locals())
