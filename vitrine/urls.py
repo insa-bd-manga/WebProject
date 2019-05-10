c5585dc03d38b580eabbb53c556a2152293fa939
MorganAsnar <morgan.asnar@insa-lyon.fr>
2019-05-10 16:21:02 +0200

    Modification de views pour plus de complétion, attribution automatique des paramètres mise à jour des urls en conséquences

--- vitrine/urls.py
+++ vitrine/urls.py
@@ -5,13 +5,33 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('recherche-ouvrages', views.rechercheOuvrages, name='recherche-ouvrages'),
+
 #   path('ouvrage/<str:auteur>/<str:titre>/<int:num_page>', views.ouvrage, name='ouvrage'),
-    path('actus/<int:num_page>/<str:tag>', views.actus, name='actus'),
+
     path('actus/<int:num_page>', views.actus, name='actus'),
     path('actus/', views.actus, name='actus'),
-    path('article/<int:id_article>/<int:num_page>', views.article, name='article'),
+
+    path('article/<int:id_article>/<int:num_commentaire>', views.article, name='article'),
+    path('article/<int:id_article>', views.article, name='article'),
+    path('article', views.article, name='article'),
+
     path('festival/<int:num_page>', views.festival, name='festival'),
+    path('festival', views.festival, name='festival'),
+
+    path('archives/<str:tag>/<int:year>/<int:month>/<int:num_page>', views.archives, name='archives'),
+    path('archives/<str:tag>/<int:year>', views.archives, name='archives'),
+    path('archives/<int:year>/<int:month>', views.archives, name='archives'),
+    path('archives/<int:year>', views.archives, name='archives'),
+    path('archives/<int:month>', views.archives, name='archives'),
+    path('archives/<str:tag>', views.archives, name='archives'),
     path('archives', views.archives, name='archives'),
+    path('archives/<str:tag>/<int:year>/<int:num_page>', views.archives, name='archives'),
+    path('archives/<int:year>/<int:month>/<int:num_page>', views.archives, name='archives'),
+    path('archives/<int:year>/<int:num_page>', views.archives, name='archives'),
+    path('archives/<int:month>/<int:num_page>', views.archives, name='archives'),
+    path('archives/<str:tag>/<int:num_page>', views.archives, name='archives'),
+    path('archives/<int:num_page>', views.archives, name='archives'),
+
     path('infos', views.infos, name='infos'),
     path('contact', views.contact, name='contact'),
 ]
--- vitrine/views.py
+++ vitrine/views.py
@@ -7,6 +7,9 @@ from vitrine.models import Article, Commentaire, Tag
 #Pour des query plus complexes que le set de base
 from django.db.models import Q
 
+#Pour la data
+from django.utils import timezone
+
 #pour les formulaires
 #from .forms import
 
@@ -56,7 +59,7 @@ def rechercheOuvrages(request):
 #     return render(request, 'vitrine/ouvrage.html', {"livres": query, "page": num_page})
 
 
-def actus(request, num_page=0, tag=""):
+def actus(request, num_page=0):
     """La page actus affiche les n dernières actus du site
 
     :param request : OSEF
@@ -71,32 +74,32 @@ def actus(request, num_page=0, tag=""):
 
     #Récupération des n articles avec le tag spécifié
     n = 10
-    query = Article.objects.filter(tag__nom_tag__contains=tag).order_by("-date")[n*(num_page):n*(num_page+1)]
+    query = Article.objects.all().order_by("-date")[n*(num_page):n*(num_page+1)]
     return render(request, 'vitrine/actus.html', {"last_articles": query, "page" : num_page})
     #/!\ définition valide pour des affichages page par page, mais apparement aussi pour de l'infinite scroll (cf : https://infinite-scroll.com/)
 
 
-def article(request, id_article, num_page):
+def article(request, id_article=1, num_commentaire=0):
     """page d'un article sépicifique, avec les commentaires en dessous
 
     page composée de l'article avec l'id_article donné, et des commentaires qui y sont liées, triées par date de publication
 
     :param request : OSEF
     :param id_article : l'id de l'article à afficher
-    :param num_page : La page a afficher, définit l'ancienneté des commentaires qu'on va afficher; les n premiers, puis les n suivants, etc
+    :param num_commentaire : La page a afficher, définit l'ancienneté des commentaires qu'on va afficher; les n premiers, puis les n suivants, etc
     :type request : requête HTTP
     :type id_article : int
-    :type num_page : int
+    :type num_commentaire : int
 
     :return paquet http contenant la page"""
 
     article=get_object_or_404(Article, id=id_article)
     n=10
-    query = Commentaire.objects.filter(id_article=id_article).order_by("-date")[n*(num_page):n*(num_page+1)]
-    return render(request, 'vitrine/article.html', {"article": article, "commentaires": query, "page": num_page})
+    query = Commentaire.objects.filter(id_article=id_article).order_by("-date")[n*(num_commentaire):n*(num_commentaire+1)]
+    return render(request, 'vitrine/article.html', {"article": article, "commentaires": query, "page": num_commentaire})
 
 
-def festival(request, num_page):
+def festival(request, num_page=0):
     """page sur le festival et le concours BD
 
         page composée d'une description statique, puis des articles tagués "festival" et "concours"
@@ -109,12 +112,31 @@ def festival(request, num_page):
     #Récupère les n articles qui ont le tag "festival" ou "concours", met en premier ceux qui sont épinglés, et trie ensuite par date
     n=10
     query = Article.objects.filter(Q(tag="festival") | Q(tag="concours")).order_by("epingler", "-date")[n*(num_page):n*(num_page+1)]
-    return render(request, 'vitrine/festival.html', {"last_articles": query, "page" : num_page})
+    return render(request, 'vitrine/festival.html', {"last_articles": query, "page": num_page})
     #/!\ définition valide pour des affichages page par page, mais apparement aussi pour de l'infinite scroll (cf : https://infinite-scroll.com/)
 
 
-def archives(request):
-    return 1
+def archives(request, tag="", year=timezone.now().year, month=timezone.now().month, num_page=0):
+    """La page archive affiche les articles du site triées par tag/date
+
+        :param request : OSEF
+        :param tag : Le tag qu'on recherche éventuellement, peut être ""
+        :param year : année de recherche des articles
+        :param month : mois de recherche des articles
+        :param num_page : La page a afficher, définit l'ancienneté des articles qu'on va afficher; les n premiers, puis les n suivants, etc
+            (n entier modulable, définit plus loin)
+        :type request : requête http
+        :type tag: String
+        :type num_page : int
+        :type year : int
+        :type month :int
+
+        :return paquet http contenant la page"""
+
+    # Récupération des n articles avec le tag et la date spécifiés
+    n = 10
+    query = Article.objects.filter(date__year=year).filter(date__month=month).filter(tag__nom_tag__contains=tag).order_by("-date")[n * (num_page):n * (num_page + 1)]
+    return render(request, 'vitrine/actus.html', {"last_articles": query, "page": num_page})
 
 
 def infos(request):
