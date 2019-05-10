62d4984df7d25130232a9f6f74320c6b7cd80e41
MorganAsnar <morgan.asnar@insa-lyon.fr>
2019-05-10 17:00:08 +0200

    Ajout du tri par tag dans la view des actus Ajout d'un filtre pour ne pas afficher les articles "du futur" modification des URLs en conséquence

--- vitrine/urls.py
+++ vitrine/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
 
 #   path('ouvrage/<str:auteur>/<str:titre>/<int:num_page>', views.ouvrage, name='ouvrage'),
 
+    path('actus/<str:tag>/<int:num_page>', views.actus, name='actus'),
+    path('actus/<str:tag>', views.actus, name='actus'),
     path('actus/<int:num_page>', views.actus, name='actus'),
     path('actus/', views.actus, name='actus'),
 
