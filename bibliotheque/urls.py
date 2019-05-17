c617fd751c3eb60b4eae7c046ca751cbf7f9c49f
MorganAsnar <morgan.asnar@insa-lyon.fr>
2019-05-17 12:51:13 +0200

    Query parfaitement fonctionnelle, pour plusieurs auteurs N'affiche que le premier cependant

--- bibliotheque/urls.py
+++ bibliotheque/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('recherche', views.recherche, name='recherche')
+    path('recherche', views.recherche, name='recherche'),
+    path('recherche/<int:num_page>', views.recherche, name='recherche')
 ]
--- bibliotheque/views.py
+++ bibliotheque/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from bibliotheque.models import Book, Author, Author_to_book
-from .forms import *
+from bibliotheque.forms import *
 
-def recherche(request):
+def recherche(request, num_page=0):
     """page de "moteur de recherche d'ouvrages"
 
         page avec un formulaire permettant de rechercher les livres par genre/titre/auteur
@@ -30,18 +30,18 @@ def recherche(request):
     #Récupération des n articles avec les critères de recherches inclus en paramètres de la fonction???
     n=10
 
-    #J'ai conscience que le dernier filtre n'est pas intuitif, la conversion de la BDD laisse quelques traces, certains
-    #noms ne sont pas forcément pertinents, mais j'ai pas voulu prendre la liberté de les renommer. Ici, ID correspond aux
-    #Auteurs d'un ouvrage. Ca fonctionne, par contre, aucun doute là dessus.
-    query = Book.objects.filter(title__icontains=titre).filter(kind__icontains=genre).filter(id__name__icontains=auteur).order_by("title")
-    nombre_reponses=len(query)
-    query = query[n * (num_page):n * (num_page + 1)]
-
     #Formulaire de recherche
     form = RechercheLivreForm(request.POST or None)
     if form.is_valid():
         auteur = form.cleaned_data["auteur"]
         genre = form.cleaned_data["genre"]
         titre = form.cleaned_data["titre"]
+
+    #J'ai conscience que le dernier filtre n'est pas intuitif, la conversion de la BDD laisse quelques traces, certains
+    #noms ne sont pas forcément pertinents, mais j'ai pas voulu prendre la liberté de les renommer. Ici, ID correspond aux
+    #Auteurs d'un ouvrage. Ca fonctionne, par contre, aucun doute là dessus.
+    query = Book.objects.filter(title__icontains=titre).filter(kind__icontains=genre).filter(id__name__icontains=auteur).order_by("title")
+    nombre_reponses=len(query)
+    query = query[n * (num_page):n * (num_page + 1)]
 
     return render(request, 'bibliotheque/recherche.html', locals())
